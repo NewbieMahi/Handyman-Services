050d4246fe90d4701e7d5ce22015113fabd2f07b
Mahesh Thorat <98503417+NewbieMahi@users.noreply.github.com>
2023-03-19 23:06:01 +0530

    Added chatbot for user help

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
 
   title = 'major-project';
+  ngOnInit() {
+
+    //----------- chatbot's code --------------//
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "1e968ac68b4a89224f4cb7f9cc4c6d19e",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      (window as any).kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+    //----------- chatbot's code end --------------//
+
+  }
 }
