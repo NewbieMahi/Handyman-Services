a22923e1d80b1ac1d2453daff0fa827bca7cca36
Mahesh Thorat <98503417+NewbieMahi@users.noreply.github.com>
2023-04-01 11:31:16 +0530

    User registration message shown

--- src/app/user-register/user-register.component.ts
+++ src/app/user-register/user-register.component.ts
@@ -32,24 +32,79 @@ emailFormControl = new FormControl('', [
 
   matcher = new MyErrorStateMatcher();
    signin: FormGroup = new FormGroup({
-    name: new FormControl('',[Validators.required ]),
-    email: new FormControl('', [Validators.email, Validators.required ]),
-    mobileNumber: new FormControl('',[Validators.required ]),
-    password: new FormControl('', [Validators.required, Validators.min(3) ]),
-    confirmPassword: new FormControl('',[Validators.required ])
+    // name: new FormControl('',[Validators.required ]),
+    // email: new FormControl('', [Validators.email, Validators.required ]),
+    // mobileNumber: new FormControl('',[Validators.required ]),
+    // password: new FormControl('', [Validators.required, Validators.min(3) ]),
+    // confirmPassword: new FormControl('',[Validators.required ])
+    name: new FormControl(''),
+    email: new FormControl(''),
+    mobileNumber: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+
   });
   hide = true;
-  get emailInput() { return this.signin.get('email'); }
-  get passwordInput() { return this.signin.get('password'); }
+  
+  isRegistrationSuccessful = false;
+  registrationErrorMessage = '';
+  isRegistering = false;
+  submitted = true;
+  snackBar: any;
+
+
+  // get emailInput() { return this.signin.get('email'); }
+  // get passwordInput() { return this.signin.get('password'); }
   constructor(@Inject(forwardRef(() => UserService)) private userService: UserService) { }
 
   registerUser() {
     
+    // const formData = this.signin.value;
+    // this.userService.register(formData.name, formData.email, formData.mobileNumber, formData.password, formData.confirmPassword)
+    // .subscribe(
+    //   response => { 
+        
+    //     console.log('User registration successful:', response);
+    //     this.isRegistrationSuccessful = true; // set the flag to true
+    //     this.registrationErrorMessage = ''; // reset the error message
+    //     this.signin.reset(); // reset the form
+    //     this.submitted = false;
+    //     this.snackBar.open('User registered successfully', 'Close', {
+    //       duration: 3000
+    //     });
+    
+    //    },
+    //   error => {
+    //     console.error('Error registering user:', error);
+    //     this.registrationErrorMessage = error.message;
+    //     this.snackBar.open('Error registering user', 'Close', {
+    //       duration: 3000
+    //     }); 
+    //   }
+    // );
     const formData = this.signin.value;
-    this.userService.register(formData.name, formData.email, formData.mobileNumber, formData.password, formData.confirmPassword)
+  this.userService.register(formData.name, formData.email, formData.mobileNumber, formData.password, formData.confirmPassword)
     .subscribe(
-      response => console.log('User registration successful:', response),
-      error => console.error('Error registering user:', error)
+      response => {
+        console.log('User registration successful:', response);
+        this.isRegistrationSuccessful = true;
+        this.registrationErrorMessage = '';
+        this.signin.reset();
+        this.submitted = false;
+        this.snackBar.open('User registered successfully', 'Close', {
+          duration: 3000
+        });
+        // Reset validation errors and set form to initial state
+        this.signin.markAsPristine();
+        this.signin.markAsUntouched();
+        Object.keys(this.signin.controls).forEach(key => {
+          this.signin.controls[key].updateValueAndValidity();
+        });
+      },
+      error => {
+        console.error('Error registering user:', error);
+        this.registrationErrorMessage = error.message || 'An unknown error occurred';
+      }
     );
     
   }
