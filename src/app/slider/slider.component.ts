4c8eb71c48d8c65bd55d313d3ef84ae14fa0cb97
Mahesh Thorat <98503417+NewbieMahi@users.noreply.github.com>
2023-04-21 11:50:52 +0530

    Updated login page

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FaqComponent } from '../faq/faq.component';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
