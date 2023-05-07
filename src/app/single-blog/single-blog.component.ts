b37a70eed6235506939bfc7e6bf9f1d95c851ef2
Mahesh Thorat <98503417+NewbieMahi@users.noreply.github.com>
2023-05-07 09:39:48 +0530

    added microphone to blog

--- src/app/single-blog/single-blog.component.ts
+++ src/app/single-blog/single-blog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BloggingService } from '../blogging.service';
+
+declare var webkitSpeechRecognition: any;
+
 @Component({
   selector: 'app-single-blog',
   templateUrl: './single-blog.component.html',
@@ -9,6 +12,8 @@ import { BloggingService } from '../blogging.service';
 export class SingleBlogComponent implements OnInit {
 
   blog: any;
+  listening: boolean = false;
+  recognition: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,4 +34,48 @@ export class SingleBlogComponent implements OnInit {
     );
   }
 
+  startListening() {
+    this.listening = true;
+    this.recognition = new webkitSpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+
+    this.recognition.onresult = (event: { resultIndex: any; results: string | any[]; }) => {
+      let interimTranscript = '';
+      let finalTranscript = '';
+
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          finalTranscript += event.results[i][0].transcript;
+        } else {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+
+      if (finalTranscript !== '') {
+        let msg = new SpeechSynthesisUtterance(finalTranscript);
+        window.speechSynthesis.speak(msg);
+      }
+    };
+
+    this.recognition.onend = () => {
+      this.listening = false;
+    }
+
+    this.recognition.start();
+    this.readDescription();
+  }
+
+  stopListening() {
+    this.listening = false;
+    window.speechSynthesis.cancel();
+    this.recognition.stop();
+  }
+
+  readDescription() {
+    if ('speechSynthesis' in window) {
+      let msg = new SpeechSynthesisUtterance(this.blog.description);
+      window.speechSynthesis.speak(msg);
+    }
+  }
 }
