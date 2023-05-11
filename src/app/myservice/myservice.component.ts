edac797b82293b9cf3a6b39e0d6cf243d0e2489c
Mahesh Thorat <98503417+NewbieMahi@users.noreply.github.com>
2023-05-11 11:04:02 +0530

    Added css styling to myservices

--- src/app/myservice/myservice.component.ts
+++ src/app/myservice/myservice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { WorkerChartService } from '../worker-chart.service';
 
@@ -8,33 +9,54 @@ import { WorkerChartService } from '../worker-chart.service';
   styleUrls: ['./myservice.component.css']
 })
 export class MyserviceComponent implements OnInit {
+
   currentUser: any;
   bookings: any = [];
+  workers: any = [];
+  filteredBookings: any = [];
+
+  searchValue: string = '';
 
   constructor(
     private authService: AuthService,
     private workerService: WorkerChartService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe((user) => {
+    this.authService.currentUser.subscribe(user => {
       this.currentUser = user;
     });
 
     this.workerService.myServices(this.currentUser.id).subscribe((data: any) => {
       this.bookings = data;
+      this.filteredBookings = data;
       for (const booking of this.bookings) {
         this.workerService.getWorkerByworkerId(booking.workerId).subscribe(
-          (worker: any) => {
-            booking.workerName = worker.name;
-            booking.workerMobileNumber = worker.mobileNumber;
-            booking.serviceTypes = worker.services.join(', ');
+          response => {
+            booking.worker = response;
           },
-          (error: any) => {
+          error => {
             console.log('Error while fetching worker details', error);
           }
         );
       }
     });
   }
+
+  search() {
+    this.filteredBookings = this.bookings.filter((booking: any) =>
+      booking.worker.name.toLowerCase().includes(this.searchValue.toLowerCase())
+    );
+  }
+
+  viewWorker(id: string) {
+    this.router.navigate(['http://localhost:4200/service', id]);
+  }
+
+  completeService(bookingId: string) {
+    // Update booking status here
+    // Redirect to myservice page
+  }
+
 }
