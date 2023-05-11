e002975d0f288a5481fd5f36dee80949bfc9dc88
Mahesh Thorat <98503417+NewbieMahi@users.noreply.github.com>
2023-05-11 10:37:53 +0530

    booking fetched successfully

--- src/app/myservice/myservice.component.ts
+++ src/app/myservice/myservice.component.ts
@@ -1,44 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { WorkerChartService } from '../worker-chart.service';
+
 @Component({
   selector: 'app-myservice',
   templateUrl: './myservice.component.html',
   styleUrls: ['./myservice.component.css']
 })
 export class MyserviceComponent implements OnInit {
-  
   currentUser: any;
   bookings: any = [];
-  workers: any = [];
 
-  constructor(private authService: AuthService,
-  private workerService: WorkerChartService,  
+  constructor(
+    private authService: AuthService,
+    private workerService: WorkerChartService,
   ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe(user => {
+    this.authService.currentUser.subscribe((user) => {
       this.currentUser = user;
     });
 
     this.workerService.myServices(this.currentUser.id).subscribe((data: any) => {
-        this.bookings = data;
-        for (const booking of this.bookings) {
-          this.workerService.getWorkerByworkerId(booking.workerId).subscribe(
-            response => {
-              console.log("res ", response);
-            },
-            error => {
-              console.log('Error while fetching worker details', error);
-            }
-          );
-        }
-  
+      this.bookings = data;
+      for (const booking of this.bookings) {
+        this.workerService.getWorkerByworkerId(booking.workerId).subscribe(
+          (worker: any) => {
+            booking.workerName = worker.name;
+            booking.workerMobileNumber = worker.mobileNumber;
+            booking.serviceTypes = worker.services.join(', ');
+          },
+          (error: any) => {
+            console.log('Error while fetching worker details', error);
+          }
+        );
+      }
     });
- 
-    
-
-  
   }
-
 }
